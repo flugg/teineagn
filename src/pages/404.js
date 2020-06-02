0e8608bc41555056dae9a31dc26b0f6df6f51e91
Alexander Tømmerås <flugged@gmail.com>
2020-06-03 01:02:23 +0200

    Updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
 
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Side ikke funnet</h1>
+      <p>Her var det ingen fisk på kroken.</p>
     </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
